de29b201fed0a6dfc9ca3065ece0e225ab34f216
alaynapuck28 <you@example.com>
2019-06-09 19:49:26 -0500

    rollodex functional

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 import './App.css';
+import UserCard from './UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      hello
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isHidden: true
+    }
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=25")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      this.setState({
+        results: json.results
+      })
+    })
+  }
+
+  onClick = () => {
+    let opposite = this.state.isHidden;
+    this.setState({
+      isHidden: !opposite
+    })
+  };
+
+    render() {
+    return (
+      <div className="App">
+        {this.state.results.map((user, index) => 
+        <UserCard 
+          key = {index}
+          firstName = {user.name.first}
+          lastName = {user.name.last}
+          picture = {user.picture.large}
+          phone = {user.cell}
+          onClick = {this.onClick} 
+          hide ={this.state.isHidden}/>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/UserCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import "./userCardStyles.css"
+
+function UserCard (props) {
+  let data;
+if (!props.hide) {
+   data = (
+    <div>
+      <h3 className = "name">{props.firstName + " " + props.lastName}</h3>
+      <p className = "phoneNumber">{props.phone}</p>
+    </div>
+  )
+} else data = null;
+
+let buttonText;
+if (props.hide) {
+  buttonText = "Show Details"
+} else buttonText = "Hide Details";
+
+
+  return (
+    <div className= "main">
+      <img className = "picture" src={props.picture}/>
+      {data}
+      <button className = "button" onClick = {props.onClick}>{buttonText}</button>
+    </div>
+  )
+}
+
+export default UserCard;
